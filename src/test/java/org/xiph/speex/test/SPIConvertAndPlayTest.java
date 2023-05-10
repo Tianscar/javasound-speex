083e48902e65e99e38eddc9038c0203326ed33c4
Karstian Lee <tianscar@protonmail.com>
2023-05-10 18:40:23 +0800

    Bug fix for SPIConvertAndPlayTest

--- src/test/java/org/xiph/speex/test/SPIConvertAndPlayTest.java
+++ src/test/java/org/xiph/speex/test/SPIConvertAndPlayTest.java
@@ -3,6 +3,8 @@ package org.xiph.speex.test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.xiph.speex.spi.SpeexEncoding;
+import org.xiph.speex.spi.SpeexFileFormatType;
 
 import javax.sound.sampled.*;
 import java.io.File;
@@ -36,15 +38,18 @@ public class SPIConvertAndPlayTest {
     public void convertWAVToSPXAndSave() throws UnsupportedAudioFileException, IOException {
         File inFile = new File("src/test/resources/fbodemo1.wav");
         System.out.println("inFile: " + inFile.getAbsolutePath());
-        AudioInputStream spxAis = AudioSystem.getAudioInputStream(inFile);
-        System.out.println("inStream: " + spxAis);
-        AudioFormat inAudioFormat = spxAis.getFormat();
+        AudioInputStream wavAis = AudioSystem.getAudioInputStream(inFile);
+        System.out.println("inStream: " + wavAis);
+        AudioFormat inAudioFormat = wavAis.getFormat();
         System.out.println("inFormat: " + inAudioFormat);
+        AudioInputStream spxAis = AudioSystem.getAudioInputStream(SpeexEncoding.SPEEX_Q10, wavAis);
         File outFile = new File("test.spx");
-        if (!outFile.getParentFile().exists()) Assertions.assertTrue(outFile.getParentFile().mkdirs());
         if (!outFile.exists()) Assertions.assertTrue(outFile.createNewFile());
         System.out.println("outFile: " + outFile.getAbsolutePath());
-        AudioSystem.write(spxAis, AudioFileFormat.Type.WAVE, outFile);
+        System.out.println("inStream: " + spxAis);
+        AudioFormat outAudioFormat = spxAis.getFormat();
+        System.out.println("inFormat: " + outAudioFormat);
+        AudioSystem.write(spxAis, SpeexFileFormatType.SPEEX, outFile);
         spxAis.close();
     }
 
@@ -52,6 +57,32 @@ public class SPIConvertAndPlayTest {
     @DisplayName("spx -> pcm, play via SPI")
     public void convertSPXToPCMAndPlay() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         File file = new File("src/test/resources/fbodemo1.spx");
+        System.out.println("inFile: " + file.getAbsolutePath());
+        AudioInputStream spxAis = AudioSystem.getAudioInputStream(file);
+        System.out.println("inStream: " + spxAis);
+        AudioFormat inAudioFormat = spxAis.getFormat();
+        System.out.println("inFormat: " + inAudioFormat);
+        AudioFormat outAudioFormat = new AudioFormat(
+                inAudioFormat.getSampleRate(),
+                16,
+                inAudioFormat.getChannels(),
+                true,
+                false);
+
+        assertTrue(AudioSystem.isConversionSupported(outAudioFormat, inAudioFormat));
+
+        AudioInputStream pcmAis = AudioSystem.getAudioInputStream(outAudioFormat, spxAis);
+        System.out.println("outStream: " + pcmAis);
+        System.out.println("outFormat: " + pcmAis.getFormat());
+
+        play(pcmAis);
+        pcmAis.close();
+    }
+
+    @Test
+    @DisplayName("ogg -> pcm, play via SPI")
+    public void convertOGGSPXToPCMAndPlay() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        File file = new File("src/test/resources/fbodemo1_speex.ogg");
         System.out.println("inFile: " + file.getAbsolutePath());
         AudioInputStream spxAis = AudioSystem.getAudioInputStream(file);
         System.out.println("inStream: " + spxAis);
